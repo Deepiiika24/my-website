05c333711eb40ff863dd3f001533ddcf0bb46a90
Deepiiika24 <deepikavenkatesan591@gmail.com>
2024-08-07 19:03:19 +0530

    aug7

--- src/Pages/Rice.jsx
+++ src/Pages/Rice.jsx
@@ -21,7 +21,7 @@ function Rice() {
         <h1 className='rice-heading'>Rice</h1>
         <ul className='rice-menu'>
           <li><Link to="/">HOME</Link></li>
-          <li style={{ fontSize: "17px", color: "white" }}> / RICE</li>
+          <li className='menu-rice'> / RICE</li>
         </ul>
       </div>
       <div className="rice-container">
@@ -34,7 +34,6 @@ function Rice() {
           <p>At Koogul Industries, we take pride in exporting only the highest quality rice to meet the diverse needs of our global customers. Our commitment to excellence begins with carefully selecting the finest rice varieties from trusted growers. Each grain undergoes rigorous quality checks to ensure it meets our exacting standards.</p>
         </div>
         <div className="rice-sub-container">
-          <div className="rice-image">
             <div className='card'>
               <img src={rice1} className='rice-pic'></img>
               <h3 className='rice-link1-name' id='rice-link-name'><Link to='/Basmati-rice'>Basmati Rice</Link> </h3>
@@ -51,8 +50,6 @@ function Rice() {
               <img src={rice4} className='rice-pic'></img>
               <h3 className='rice-link1-name' id='rice-link-name'><Link to='/Boiled-rice'>Boiled Rice</Link> </h3>
             </div>
-          </div>
-          <div className="rice-image1">
             <div className='card'>
               <img src={rice5} className='rice-pic'></img>
               <h3 className='rice-link1-name' id='rice-link-name'><Link to='/Red-raw-rice'>Red Raw Rice</Link> </h3>
@@ -65,7 +62,6 @@ function Rice() {
               <img src={rice7} className='rice-pic'></img>
               <h3 className='rice-link1-name' id='rice-link-name'><Link to='/White-raw-rice'>White Raw Rice</Link> </h3>
             </div>
-          </div>
         </div>
       </div>
     </div>
